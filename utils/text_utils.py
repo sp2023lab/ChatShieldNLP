f8fb80df4be0d1a6b61432c6e225dd9574dfed3a
Shyam Popat <shyampopat2023@gmail.com>
2025-08-15 15:17:50 +0100

    Third prototype

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -6,11 +6,13 @@ def normalize_text(text: str) -> str:
     text = re.sub(r'[^\x00-\x7F]+', '', text)
     text = " ".join(text.split())
     text = text.strip()
+    print(f"Here is the text {text}")
     return text
 
 def tokenize(text: str) -> list[str]:
     normalized = normalize_text(text)
     tokens = normalized.split()
+    print(f"Here is the tokens {tokens}")
     return tokens  
 
 def highlight_detected_phrases(text: str, phrases: list[str]) -> str:
@@ -18,4 +20,5 @@ def highlight_detected_phrases(text: str, phrases: list[str]) -> str:
     for phrase in phrases:
         pattern = re.compile(re.escape(phrase), re.IGNORECASE)
         html_string = pattern.sub(lambda m: f"<span style='color:red'>{m.group(0)}</span>", html_string)
+    print(f"Here is the HTML string: {html_string}")
     return html_string
